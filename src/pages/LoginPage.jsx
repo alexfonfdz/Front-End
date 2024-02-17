436f0a2dac5ac028cd6e3ab0d7c2bd14baf8eb1b
luism <luismirandahola@gmail.com>
2024-02-17 16:09:39 -0700

    password field fixed

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -43,7 +43,7 @@ function LoginPage() {
   }
 
   return (
-      <div style={{ background: 'rgb(251, 225, 147)', width: 'auto', height: 'auto', padding: '75px', borderRadius: '10px', border: '2px solid var(--bs-emphasis-color)', marginTop: '60px' }}>
+      <div style={{ background: 'rgb(251, 225, 147)', width: 'auto', height: 'auto', padding: '75px', borderRadius: '10px', border: '2px solid var(--bs-emphasis-color)', marginTop: '30px' }}>
         <div style={{ width: '200px', textAlign: 'center' }}>
           <div style={{ textAlign: 'center' }}><img style={{ height: '100px', borderRadius: '10px', width: '100px', background: 'url(\'src/assets/img/pollomaxlogo.jpg\')', minWidth: 'auto', borderWidth: '10px' }} src="src/assets/img/pollomaxlogo.jpg" alt="Logo"/></div>
           <div>
@@ -53,13 +53,14 @@ function LoginPage() {
             <p style={{ marginTop: '10px', fontFamily: 'Allerta' }}>Usuario:</p>
           </div>
           <div>
-            <div className="d-flex justify-content-center" style={{ borderRadius: '10px', color: 'rgb(157,153,153)', background: '#dadada', padding: '0px', border: '2px solid rgb(0,0,0)'}}><input type="text" style={{ color: 'rgb(0, 0, 0)', background: 'rgba(255,255,255,0)', borderColor: 'transparent', outline: 'none',  padding: '10px', borderRadius: '10px'}} /></div>
+            <div className="d-flex justify-content-center" style={{ borderRadius: '10px', color: 'rgb(157,153,153)', background: '#dadada', padding: '0px', border: '2px solid rgb(0,0,0)'}}>
+              <input type="text" style={{ color: 'rgb(0, 0, 0)', background: 'rgba(255,255,255,0)', borderColor: 'transparent', outline: 'none',  padding: '10px', borderRadius: '10px'}} /></div>
           </div>
           <div>
             <p style={{ marginTop: '10px', fontFamily: 'Allerta' }}>Contraseña:</p>
           </div>
           <div className="d-flex justify-content-center" style={{ position: 'relative', borderRadius: '10px', color: 'rgb(157,153,153)', background: '#dadada', padding: '0px', border: '2px solid rgb(0,0,0)', alignItems: 'center'}}>
-            <input type={pass} style={{ color: 'rgb(0,0,0)', background: 'rgba(255,255,255,0)', borderColor: 'rgba(194,186,186,0)', outline: 'none', padding: '10px', borderRadius: '10px', marginRight: '25```px', width:'175px'}} />
+            <input type={pass} style={{ color: 'rgb(0,0,0)', background: 'rgba(255,255,255,0)', borderColor: 'rgba(194,186,186,0)', outline: 'none', padding: '10px', borderRadius: '10px', marginRight: '25px', width:'175px'}} />
             <input name='viewPass' type='checkbox' onChange={handleCheck} style={{ position: 'absolute', right: '10px', top: '50%', transform: 'translateY(-50%)', backgroundColor: 'rgb(255,255,255)', height: '20px', width: '20px'}}/>
         </div>
           <div><button className="btn btn-primary" type="button" style={{ marginTop: '20px', fontFamily: 'Allerta', background: color.color, borderWidth: '5px', borderColor: color.borderColor, borderTopColor: color.borderTopColor, borderRightColor: color.borderRightColor, borderBottomColor: color.borderBottomColor, outline: 'none' }} onMouseUp={handleMouseUp} onMouseDown={handleMouseDown}>Entrar</button></div>

f206e5d75d6839d8ed46c575724efa91a4a0fb2c
alexfonfdz <fofapersonal@gmail.com>
2024-02-18 10:01:48 -0700

    Detallito del menu-sidebar

--- src/pages/menuPage.jsx
+++ src/pages/menuPage.jsx
@@ -4,12 +4,11 @@ import SideBar from '../../components/ResponsiveAppBar';
 export default function MenuPage() {
   return (
     <div>
-      <SideBar>
+      <SideBar/>
     <div className="menuPage">
       <h1>Menu</h1>
       <p>Menu page content</p>
     </div>
-    </SideBar>
     </div>
   );
 }

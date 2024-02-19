ae3497f434ca1d4905eb9104da6057d9214ce58b
alexfonfdz <fofapersonal@gmail.com>
2024-02-19 09:15:33 -0700

    PopUp para datos de login

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -4,6 +4,7 @@ import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 import './LoginPage.css'
 import useUser from '../useUser';
+import Popup from '../../components/PopUp';
 
 export default function LoginPage() {
   const nav = useNavigate();
@@ -11,6 +12,17 @@ export default function LoginPage() {
   const [user, setUser] = useState('')
   const [pass, setPass] = useState('')
   const {userInfo, setUserInfo} = useUser();
+  const [color, setColor] = useState({
+    color: 'rgba(117, 42, 116, 1)',
+    borderColor: 'rgba(117, 42, 116, 1)',
+    borderTopColor: 'rgba(117, 42, 116, 1)',
+    borderRightColor: 'rgba(117, 42, 116, 1)',
+    borderBottomColor: 'rgba(117, 42, 116, 1)',
+  })
+
+  const [popupMessage, setPopupMessage] = useState('');
+  const [popupImage, setPopupImage] = useState('');
+  const [showPopup, setShowPopup] = useState(false);
 
   const handleLogin = async () => {
     
@@ -24,7 +36,6 @@ export default function LoginPage() {
       console.log('Response:', response);
 
       if (response.status === 200) {
-        alert('Login successful');
         console.log(userInfo)
         if(response.data){
           setUserInfo(response.data);
@@ -34,10 +45,15 @@ export default function LoginPage() {
         // Redirect to home page
         nav("/menu")
       } else {
-        alert('Login failed:', response.data.message);
+        setPopupImage("../src/assets/img/error.png");
+        setPopupMessage('Login failed:' + response.data.message);
+        setShowPopup(true);
       }
     } catch (error) {
-      alert('Error during login:', error);    }
+      setPopupImage("../src/assets/img/error.png");
+      setPopupMessage(error.response.data.message);   
+      setShowPopup(true);
+    }
   }
   
 
@@ -49,13 +65,10 @@ export default function LoginPage() {
     }
     }
 
-  const [color, setColor] = useState({
-    color: 'rgba(117, 42, 116, 1)',
-    borderColor: 'rgba(117, 42, 116, 1)', 
-    borderTopColor: 'rgba(117, 42, 116, 1)', 
-    borderRightColor: 'rgba(117, 42, 116, 1)', 
-    borderBottomColor: 'rgba(117, 42, 116, 1)',
-  }) 
+  
+  const handleClosePopup = () => {
+    setShowPopup(false);
+  }
 
   function handleMouseUp(){
     setColor({
@@ -74,6 +87,16 @@ export default function LoginPage() {
       borderTopColor: 'rgba(155,80,154, 1)', 
       borderRightColor: 'rgba(155, 80,154,1)', 
       borderBottomColor: 'rgba(155, 80,154,1)',
+    })
+  }
+
+  function handleHover(){
+    setColor({
+      color: 'rgba(155, 80, 154, 0.5)',
+      borderColor: 'rgba(155, 80, 154, 0.5)', 
+      borderTopColor: 'rgba(155,80,154, 0.5)', 
+      borderRightColor: 'rgba(155, 80,154,0.5)', 
+      borderBottomColor: 'rgba(155, 80,154,0.5)',
     })
   }
 
@@ -99,11 +122,12 @@ export default function LoginPage() {
             <input value={pass} onChange={(e) => setPass(e.target.value)} type={password} style={{ color: 'rgb(0,0,0)', background: 'rgba(255,255,255,0)', borderColor: 'rgba(194,186,186,0)', outline: 'none', padding: '10px', borderRadius: '10px', marginRight: '25px', width:'175px'}} />
             <input name='viewPass' type='checkbox' onChange={handleCheck} style={{ position: 'absolute', right: '10px', top: '50%', transform: 'translateY(-50%)', backgroundColor: 'rgb(255,255,255)', height: '20px', width: '20px'}}/>
         </div>
-          <div><button onClick={handleLogin} className="btn btn-primary" type="button" style={{ marginTop: '20px', fontFamily: 'Allerta', background: color.color, borderWidth: '5px', borderColor: color.borderColor, borderTopColor: color.borderTopColor, borderRightColor: color.borderRightColor, borderBottomColor: color.borderBottomColor, outline: 'none' }} onMouseUp={handleMouseUp} onMouseDown={handleMouseDown}>Entrar</button></div>
+          <div><button onClick={handleLogin} className="btn btn-primary" type="button" style={{ marginTop: '20px', fontFamily: 'Allerta', background: color.color, borderWidth: '5px', borderColor: color.borderColor, borderTopColor: color.borderTopColor, borderRightColor: color.borderRightColor, borderBottomColor: color.borderBottomColor, outline: 'none' }} onMouseUp={handleMouseUp} onMouseDown={handleMouseDown} onMouseOver={handleHover} onMouseLeave={handleMouseUp}>Entrar</button></div>
         </div>
         <div>
         </div>
       </div>
+      {showPopup && <Popup image={popupImage} message={popupMessage} onClose={handleClosePopup} />}
       </div>
   );
 }

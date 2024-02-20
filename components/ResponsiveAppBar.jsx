6f14c2ccbca930ccdea80083b391d53c9f4320d7
alexfonfdz <fofapersonal@gmail.com>
2024-02-20 08:50:32 -0700

    Login Enter - Navbar casi se puede lo de cambiar el color al seleccionar una opcion

--- components/ResponsiveAppBar.jsx
+++ components/ResponsiveAppBar.jsx
@@ -1,76 +1,73 @@
-//Barra de navegación responsiva
-
 import { Link } from 'react-router-dom';
-// import { useState } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import useUser from '../src/useUser';
 import './ResponsiveAppBar.css'; // Import your CSS file
 
-
-
-export default function Sidebar({ isSidebarOpen, setIsSidebarOpen}){
+export default function Sidebar({ isSidebarOpen, setIsSidebarOpen}) {
     const { userInfo, handleLogout } = useUser();
+    const [selectedPage, setSelectedPage] = useState('');
+
     let pages = [];
-    console.log(userInfo)
-    const pageNames = ['Productos', 'Transacciones', 'Inventario', 'Reportes'];
+    const pageNames = ['Menú','Productos', 'Transacciones', 'Inventario', 'Reportes'];
+
     const toggleSidebar = () => {
         setIsSidebarOpen(!isSidebarOpen);
     };
 
     if(userInfo.userTypeName === 'ADMINISTRADOR'){
-      pages = ['admin-productos', 'transacciones', 'inventario', 'reportes'];
-    }else{
-      pages = ['productos'];
+        pages = ['menu','admin-productos', 'transacciones', 'inventario', 'reportes'];
+    } else {
+        pages = ['menu','productos'];
     }
+
+    const handlePageClick = (page) => {
+      setSelectedPage(page);
+    };
 
     return (
         <div>
-<div className="wrapper">
-      <div id="mySidebar" className={`sidebar ${isSidebarOpen ? '' : 'closed'}`}  style={{ background: "#fbd23a", height: '100%', width: '250px' }}>
-      <button className="sidebar-toggle" onClick={toggleSidebar}>
-            {isSidebarOpen ? '×' : '☰'}
-        </button>
-        <div className="sidebar-header">
-          <img
-            src="../src/assets/img/pollomaxlogo.jpg"
-            style={{
-              width: "80px",
-              height: "80px",
-              borderRadius: "30px",
-              margin: "0px 0px 0px 30px"
-            }}
-            alt="Logo"
-          />
+            <div className="wrapper">
+                <div id="mySidebar" className={`sidebar ${isSidebarOpen ? '' : 'closed'}`} style={{ background: "#fbd23a", height: '100%', width: '250px' }}>
+                    <button className="sidebar-toggle" onClick={toggleSidebar}>
+                        {isSidebarOpen ? '×' : '☰'}
+                    </button>
+                    <div className="sidebar-header">
+                        <img
+                            src="../src/assets/img/pollomaxlogo.jpg"
+                            style={{
+                                width: "80px",
+                                height: "80px",
+                                borderRadius: "30px",
+                                margin: "0px 0px 0px 30px"
+                            }}
+                            alt="Logo"
+                        />
+                    </div>
+                    <ul className="list-unstyled components" style={{ '--bs-primary': '#fbd23a', '--bs-primary-rgb': '251,210,58', background: '#fbd23a', borderColor: '#fbd23a' }}>
+                        {pages.map((page, index) => (
+                            <li key={index} style={{ backgroundColor: selectedPage===page ? 'rgb(124,50,113)':'#fbd23a', color: selectedPage===page ? 'white':'var(--bs-emphasis-color)', fontSize: '20px', fontFamily: 'Allerta', fontWeight: 'bold' }}>
+                                <Link to={`/${page}`} onClick={() => handlePageClick(page)}>{pageNames[index]}</Link>
+                            </li>
+                        ))}
+                        <div>
+                            <button className='close-session' onClick={handleLogout}>
+                                Cerrar sesión, {userInfo.username}
+                            </button>
+                        </div>
+                    </ul>
+                </div>
+            </div>
+            <button className="sidebar-toggle" onClick={toggleSidebar}>
+                {isSidebarOpen ? '×' : '☰'}
+            </button>
         </div>
-        <ul className="list-unstyled components" style={{ '--bs-primary': '#fbd23a', '--bs-primary-rgb': '251,210,58', background: '#fbd23a', borderColor: '#fbd23a' }}>
-          <li style={{ color: 'var(--bs-emphasis-color)', fontSize: '20px', fontFamily: 'Allerta', fontWeight: 'bold' }}>
-            <Link to="/menu">Menú</Link>
-          </li>
-          {pages.map((page, index) => (
-              <li key={index} style={{ color: 'var(--bs-emphasis-color)', fontSize: '20px', fontFamily: 'Allerta', fontWeight: 'bold' }}>
-                <Link to={`/${page}`}>{pageNames[index]}</Link>
-              </li>
-          ))}
-          <div>
-        <button className='close-session' onClick={handleLogout}>
-            Cerrar sessión, {userInfo.username}
-        </button>
-    </div>
-        </ul>
-      </div>
-    </div>
-    <button className="sidebar-toggle" onClick={toggleSidebar}>
-            {isSidebarOpen ? '×' : '☰'}
-    </button>
-    </div>
-
-
     );
-    
 }
 
 Sidebar.propTypes = {
-  isSidebarOpen: PropTypes.bool.isRequired,
-  setIsSidebarOpen: PropTypes.func.isRequired,
+    isSidebarOpen: PropTypes.bool.isRequired,
+    setIsSidebarOpen: PropTypes.func.isRequired,
 };
+
 
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,4 +1,3 @@
-//Pagina de login
 import { useState } from 'react'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
@@ -25,7 +24,6 @@ export default function LoginPage() {
   const [showPopup, setShowPopup] = useState(false);
 
   const handleLogin = async () => {
-    
     const loginData = {
       user,
       pass
@@ -100,9 +98,15 @@ export default function LoginPage() {
     })
   }
 
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      handleLogin();
+    }
+  }
+
   return (
     <div id="login-container" className="text-center d-lg-flex flex-fill justify-content-center align-items-center align-content-center align-self-center flex-nowrap justify-content-lg-center align-items-lg-center" style={{background: 'rgb(250, 240, 210)', borderColor: 'rgb(117, 42, 116)', borderTopColor: 'rgb(117, 42, 116)', borderRightColor: 'rgb(42, 116, 42)', borderBottomColor: 'rgb(42, 116, 116)', borderLeftColor: 'rgb(42, 116, 42)'}}  >
-      <div style={{ background: 'rgb(251, 225, 147)', width: 'auto', height: 'auto', padding: '75px', borderRadius: '10px', border: '2px solid var(--bs-emphasis-color)', marginTop: '30px' }}>
+      <form id='login-form' style={{ background: 'rgb(251, 225, 147)', width: 'auto', height: 'auto', padding: '75px', borderRadius: '10px', border: '2px solid var(--bs-emphasis-color)', marginTop: '30px' }}>
         <div style={{ width: '200px', textAlign: 'center' }}>
           <div style={{ textAlign: 'center' }}><img style={{ height: '100px', borderRadius: '10px', width: '100px', background: 'url(\'src/assets/img/pollomaxlogo.jpg\')', minWidth: 'auto', borderWidth: '10px' }} src="src/assets/img/pollomaxlogo.jpg" alt="Logo"/></div>
           <div>
@@ -113,21 +117,22 @@ export default function LoginPage() {
           </div>
           <div>
             <div className="d-flex justify-content-center" style={{ borderRadius: '10px', color: 'rgb(157,153,153)', background: '#dadada', padding: '0px', border: '2px solid rgb(0,0,0)'}}>
-              <input value={user} onChange={(e) => setUser(e.target.value)} type="text" style={{ color: 'rgb(0, 0, 0)', background: 'rgba(255,255,255,0)', borderColor: 'transparent', outline: 'none',  padding: '10px', borderRadius: '10px'}} /></div>
+              <input value={user} onChange={(e) => setUser(e.target.value)} type="text" style={{ color: 'rgb(0, 0, 0)', background: 'rgba(255,255,255,0)', borderColor: 'transparent', outline: 'none',  padding: '10px', borderRadius: '10px'}} onKeyDown={handleKeyDown} /></div>
           </div>
           <div>
             <p style={{ marginTop: '10px', fontFamily: 'Allerta' }}>Contraseña:</p>
           </div>
           <div className="d-flex justify-content-center" style={{ position: 'relative', borderRadius: '10px', color: 'rgb(157,153,153)', background: '#dadada', padding: '0px', border: '2px solid rgb(0,0,0)', alignItems: 'center'}}>
-            <input value={pass} onChange={(e) => setPass(e.target.value)} type={password} style={{ color: 'rgb(0,0,0)', background: 'rgba(255,255,255,0)', borderColor: 'rgba(194,186,186,0)', outline: 'none', padding: '10px', borderRadius: '10px', marginRight: '25px', width:'175px'}} />
+            <input value={pass} onChange={(e) => setPass(e.target.value)} type={password} style={{ color: 'rgb(0,0,0)', background: 'rgba(255,255,255,0)', borderColor: 'rgba(194,186,186,0)', outline: 'none', padding: '10px', borderRadius: '10px', marginRight: '25px', width:'175px'}} onKeyDown={handleKeyDown} />
             <input name='viewPass' type='checkbox' onChange={handleCheck} style={{ position: 'absolute', right: '10px', top: '50%', transform: 'translateY(-50%)', backgroundColor: 'rgb(255,255,255)', height: '20px', width: '20px'}}/>
         </div>
-          <div><button onClick={handleLogin} className="btn btn-primary" type="button" style={{ marginTop: '20px', fontFamily: 'Allerta', background: color.color, borderWidth: '5px', borderColor: color.borderColor, borderTopColor: color.borderTopColor, borderRightColor: color.borderRightColor, borderBottomColor: color.borderBottomColor, outline: 'none' }} onMouseUp={handleMouseUp} onMouseDown={handleMouseDown} onMouseOver={handleHover} onMouseLeave={handleMouseUp}>Entrar</button></div>
+          <div><button form='login-form' onClick={handleLogin} className="btn btn-primary" type="button" style={{ marginTop: '20px', fontFamily: 'Allerta', background: color.color, borderWidth: '5px', borderColor: color.borderColor, borderTopColor: color.borderTopColor, borderRightColor: color.borderRightColor, borderBottomColor: color.borderBottomColor, outline: 'none' }} onMouseUp={handleMouseUp} onMouseDown={handleMouseDown} onMouseOver={handleHover} onMouseLeave={handleMouseUp}>Entrar</button></div>
         </div>
         <div>
         </div>
-      </div>
+      </form>
       {showPopup && <Popup image={popupImage} message={popupMessage} onClose={handleClosePopup} />}
       </div>
   );
 }
+

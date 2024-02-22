8b3a2c633eff262ab8b239fd0938b86762ea2edf
alexfonfdz <fofapersonal@gmail.com>
2024-02-22 09:41:44 -0700

    Inventario medio adaptable y menu sin borders feos

--- src/pages/admin/InventoryPage.jsx
+++ src/pages/admin/InventoryPage.jsx
@@ -1,20 +1,23 @@
-import { useState, useEffect } from 'react';
-import './InventoryPage.css';
-import Sidebar from '../../../components/ResponsiveAppBar';
-import notificacion from '../../assets/img/notificacion.png';
-import axios from 'axios';
-import EditMinimumAmountPopup from '../../../components/PopUpInventory';
+import { useState, useEffect } from "react";
+import "./InventoryPage.css";
+import Sidebar from "../../../components/ResponsiveAppBar";
+import notificacion from "../../assets/img/notificacion.png";
+import axios from "axios";
+import EditMinimumAmountPopup from "../../../components/PopUpInventory";
 
 async function updateInventoryAmount(idProduct, minimumAmount) {
   try {
-    const response = await axios.put('http://localhost:3000/updateInventoryAmount', {
-      idProduct,
-      minimumAmount,
-    });
+    const response = await axios.put(
+      "http://localhost:3000/updateInventoryAmount",
+      {
+        idProduct,
+        minimumAmount,
+      }
+    );
 
     console.log(response.data.message);
   } catch (error) {
-    console.error('Error:', error);
+    console.error("Error:", error);
   }
 }
 
@@ -23,17 +26,17 @@ export default function InventoryPage() {
   const [items, setItems] = useState([]);
   const [isEditing, setIsEditing] = useState(false);
   const [currentItem, setCurrentItem] = useState(null);
-  const [searchTerm, setSearchTerm] = useState(''); // Cambia esto para que searchTerm sea modificable
+  const [searchTerm, setSearchTerm] = useState(""); // Cambia esto para que searchTerm sea modificable
 
   const fetchItems = async () => {
     try {
-      const response = await axios.get('http://localhost:3000/inventory');
+      const response = await axios.get("http://localhost:3000/inventory");
       setItems(response.data);
     } catch (error) {
-      console.error('There was an error!', error);
+      console.error("There was an error!", error);
     }
   };
-  
+
   const handleSearchChange = (event) => {
     setSearchTerm(event.target.value);
   };
@@ -42,18 +45,20 @@ export default function InventoryPage() {
   }, []);
 
   useEffect(() => {
-    axios.get('http://localhost:3000/inventory')
-      .then(response => {
+    axios
+      .get("http://localhost:3000/inventory")
+      .then((response) => {
         setItems(response.data);
       })
-      .catch(error => {
-        console.error('There was an error!', error);
+      .catch((error) => {
+        console.error("There was an error!", error);
       });
   }, []);
 
-  const filteredItems = items.filter((item) =>
-    item.productName.toLowerCase().includes(searchTerm.toLowerCase()) ||
-    item.idProduct.toString().includes(searchTerm)
+  const filteredItems = items.filter(
+    (item) =>
+      item.productName.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      item.idProduct.toString().includes(searchTerm)
   );
 
   const startEditing = (item) => {
@@ -67,28 +72,38 @@ export default function InventoryPage() {
 
   const handleSave = async (minimumAmount) => {
     await updateInventoryAmount(currentItem.idProduct, minimumAmount);
-    setItems(items.map(item => 
-      item.idProduct === currentItem.idProduct ? { ...item, minimumAmount: minimumAmount } : item
-    ));
+    setItems(
+      items.map((item) =>
+        item.idProduct === currentItem.idProduct
+          ? { ...item, minimumAmount: minimumAmount }
+          : item
+      )
+    );
     fetchItems();
     stopEditing();
   };
-  
+
   return (
     <div>
-      <Sidebar isSidebarOpen={isSidebarOpen} setIsSidebarOpen={setIsSidebarOpen} />
-      <div style={{padding: isSidebarOpen ? '25px 25px 25px 275px' : '25px'}}>    
-      <div className="div-search">
-      <input
-          type="search"
-          placeholder="Buscar producto por nombre o ID..."
-          value={searchTerm}
-          onChange={handleSearchChange}
-        />   
-      </div>   
-      <div className="table-container">
+      <Sidebar
+        isSidebarOpen={isSidebarOpen}
+        setIsSidebarOpen={setIsSidebarOpen}
+      />
+      <div style={{ padding: isSidebarOpen ? "0px 0px 0px 250px" : "0px" }}>
+        <div className="div-search">
+          <input
+            type="search"
+            placeholder="Buscar producto por nombre o ID..."
+            value={searchTerm}
+            onChange={handleSearchChange}
+          />
+        </div>
+        <div
+          className="table-container"
+          style={{ width: isSidebarOpen ? "100vw" : "100vw" }}
+        >
           <table className="table">
-          <thead style={{backgroundColor: 'purple', color: 'white'}}>
+            <thead style={{ backgroundColor: "purple", color: "white" }}>
               <tr>
                 <th>ID Producto</th>
                 <th>Tipo de Producto</th>
@@ -100,24 +115,94 @@ export default function InventoryPage() {
               </tr>
             </thead>
             <tbody>
-            {filteredItems.map((item) => (
-                <tr key={item.id} >
-                  <td style={{ backgroundColor: item.productAmount < item.minimumAmount ? '#F5B470' : '#90EE90' }}>{item.idProduct}</td>
-                  <td style={{ backgroundColor: item.productAmount < item.minimumAmount ? '#F5B470' : '#90EE90' }}>{item.idProductType}</td>
-                  <td style={{ backgroundColor: item.productAmount < item.minimumAmount ? '#F5B470' : '#90EE90' }}>{item.productName}</td>
-                  <td style={{ backgroundColor: item.productAmount < item.minimumAmount ? '#F5B470' : '#90EE90' }}>{item.productAmount}</td>
-                  <td style={{ backgroundColor: item.productAmount < item.minimumAmount ? '#F5B470' : '#90EE90' }}>{item.minimumAmount}</td>
-                  <td style={{ backgroundColor: item.productAmount < item.minimumAmount ? '#F5B470' : '#90EE90' }}>{item.unitName}</td>
-                  <td style={{ backgroundColor: item.productAmount < item.minimumAmount ? '#F5B470' : '#90EE90' }}>
-                    <img src={notificacion} onClick={() => startEditing(item)}/>
+              {filteredItems.map((item) => (
+                <tr key={item.id}>
+                  <td
+                    style={{
+                      backgroundColor:
+                        item.productAmount < item.minimumAmount
+                          ? "#F5B470"
+                          : "#90EE90",
+                    }}
+                  >
+                    {item.idProduct}
+                  </td>
+                  <td
+                    style={{
+                      backgroundColor:
+                        item.productAmount < item.minimumAmount
+                          ? "#F5B470"
+                          : "#90EE90",
+                    }}
+                  >
+                    {item.idProductType}
+                  </td>
+                  <td
+                    style={{
+                      backgroundColor:
+                        item.productAmount < item.minimumAmount
+                          ? "#F5B470"
+                          : "#90EE90",
+                    }}
+                  >
+                    {item.productName}
+                  </td>
+                  <td
+                    style={{
+                      backgroundColor:
+                        item.productAmount < item.minimumAmount
+                          ? "#F5B470"
+                          : "#90EE90",
+                    }}
+                  >
+                    {item.productAmount}
+                  </td>
+                  <td
+                    style={{
+                      backgroundColor:
+                        item.productAmount < item.minimumAmount
+                          ? "#F5B470"
+                          : "#90EE90",
+                    }}
+                  >
+                    {item.minimumAmount}
+                  </td>
+                  <td
+                    style={{
+                      backgroundColor:
+                        item.productAmount < item.minimumAmount
+                          ? "#F5B470"
+                          : "#90EE90",
+                    }}
+                  >
+                    {item.unitName}
+                  </td>
+                  <td
+                    style={{
+                      backgroundColor:
+                        item.productAmount < item.minimumAmount
+                          ? "#F5B470"
+                          : "#90EE90",
+                    }}
+                  >
+                    <img
+                      src={notificacion}
+                      onClick={() => startEditing(item)}
+                    />
                   </td>
                 </tr>
-            ))}
+              ))}
             </tbody>
           </table>
         </div>
       </div>
-      {isEditing && <EditMinimumAmountPopup onClose={stopEditing} item={currentItem} onSave={handleSave} />}
+      {isEditing && (
+        <EditMinimumAmountPopup
+          onClose={stopEditing}
+          item={currentItem}
+          onSave={handleSave}
+        />
+      )}
     </div>
   );
 }
--- src/pages/menuPage.jsx
+++ src/pages/menuPage.jsx
@@ -40,54 +40,42 @@ export default function MenuPage() {
   return (
     <div className='d-flex' style={{background: '#faf0d2', position:'initial', flexWrap:'wrap'}}>
       <Sidebar isSidebarOpen={isSidebarOpen} setIsSidebarOpen={setIsSidebarOpen} />
-      <div style={{ padding: isSidebarOpen ? '0px 240px 150px 240px' : '0px 0px 150px 0px', position:'initial' }}>
+      <div style={{ padding: isSidebarOpen ? '0px 240px 150px 240px' : '0px 0px 150px 40px', position:'initial' }}>
         <div className="wrapper" style={{ background: '#faf0d2', fontFamily: 'Allerta, sans-serif', fontSize: 22, borderTopStyle: 'none' }}>
           <div id="content" style={{ background: '#FAF0D2', borderTopWidth: 0, borderBottomWidth: 2, borderBottomColor: '#7c3271'}}>
             <div style={{ marginTop: 10, marginLeft: 30 }}>
               <h1 style={{ fontSize: 25, fontFamily: 'Allerta, sans-serif' }}>Expendio Bachoco</h1>
               <h1 style={{ fontFamily: 'Allerta, sans-serif', fontSize: 38 }}>EL POLLO MAX</h1>
             </div>
-            <div className="d-flex justify-content-between" style={{ marginTop: 100, marginLeft: 50 }}>
+            <div className="d-flex justify-content-between" style={{ marginTop: 50, marginLeft: 50 }}>
               <div className="table-responsive" style={{ background: '#faf0d2' }}>
                 <table className="table table-borderless">
                   <thead style={{ background: '#FAF0D2' }}>
                     <tr style={{ background: '#FAF0D2' }}>
-                      <th style={{ background: '#faf0d2', fontSize: 25, paddingRight: 50, borderTopWidth: 0, borderBottomWidth: 2, borderBottomColor: '#7c3271', fontFamily: 'Allerta, sans-serif' }}>Producto</th>
-                      <th style={{ background: '#faf0d2', fontSize: 25, marginRight: 0, paddingRight: 50, borderTopWidth: 0, borderBottomWidth: 2, borderBottomColor: '#7c3271', fontFamily: 'Allerta, sans-serif' }}>Cantidad</th>
-                      <th style={{ background: '#faf0d2', fontSize: 25, width: 100, paddingRight: 50, borderTopWidth: 0, borderBottomWidth: 2, borderBottomColor: '#7c3271', fontFamily: 'Allerta, sans-serif' }}>P.U.</th>
-                      <th style={{ background: '#faf0d2', fontSize: 25, marginRight: 0, paddingRight: 50, borderTopWidth: 0, borderBottomWidth: 2, borderBottomColor: '#7c3271', fontFamily: 'Allerta, sans-serif' }}>Subtotal</th>
+                      <th style={{ background: '#faf0d2', fontSize: 25, paddingRight: 50, borderTopWidth: 0, borderBottomWidth: 2, borderBottomColor: '#7c3271', fontFamily: 'Allerta, sans-serif', color:'black' }}>Producto</th>
+                      <th style={{ background: '#faf0d2', fontSize: 25, marginRight: 0, paddingRight: 50, borderTopWidth: 0, borderBottomWidth: 2, borderBottomColor: '#7c3271', fontFamily: 'Allerta, sans-serif', color:'black' }}>Cantidad</th>
+                      <th style={{ background: '#faf0d2', fontSize: 25, width: 100, paddingRight: 50, borderTopWidth: 0, borderBottomWidth: 2, borderBottomColor: '#7c3271', fontFamily: 'Allerta, sans-serif', color:'black' }}>P.U.</th>
+                      <th style={{ background: '#faf0d2', fontSize: 25, marginRight: 0, paddingRight: 50, borderTopWidth: 0, borderBottomWidth: 2, borderBottomColor: '#7c3271', fontFamily: 'Allerta, sans-serif', color:'black' }}>Subtotal</th>
                     </tr>
                   </thead>
                   <tbody>
                     <tr>
-                      <td style={{ background: '#faf0d2', height: 10 }}></td>
-                      <td style={{ background: '#faf0d2', height: 10 }}></td>
-                      <td style={{ background: '#faf0d2', width: 100, height: 10 }}></td>
-                      <td style={{ background: '#faf0d2', height: 10 }}></td>
+                      <td style={{ background: '#faf0d2', borderBottomWidth: 0, fontFamily: 'Allerta, sans-serif', fontSize: 24, border:'none' }}>Pollo</td>
+                      <td style={{ background: '#faf0d2', borderBottomWidth: 0, fontFamily: 'Allerta, sans-serif', fontSize: 24, border:'none' }}>1.809</td>
+                      <td style={{ background: '#faf0d2', width: 100, borderBottomWidth: 0, fontFamily: 'Allerta, sans-serif', fontSize: 24, border:'none' }}>$134.00</td>
+                      <td style={{ background: '#faf0d2', borderBottomWidth: 0, fontFamily: 'Allerta, sans-serif', fontSize: 24, border:'none' }}>$242.41</td>
                     </tr>
                     <tr>
-                      <td style={{ background: '#faf0d2', borderBottomWidth: 0, fontFamily: 'Allerta, sans-serif', fontSize: 24 }}>Pollo</td>
-                      <td style={{ background: '#faf0d2', borderBottomWidth: 0, fontFamily: 'Allerta, sans-serif', fontSize: 24 }}>1.809</td>
-                      <td style={{ background: '#faf0d2', width: 100, borderBottomWidth: 0, fontFamily: 'Allerta, sans-serif', fontSize: 24 }}>$134.00</td>
-                      <td style={{ background: '#faf0d2', borderBottomWidth: 0, fontFamily: 'Allerta, sans-serif', fontSize: 24 }}>$242.41</td>
+                      <td style={{ background: '#faf0d2', borderTopColor: '#7c3271', borderLeft:'0px',borderRight:'0px', borderBottom:'0px', borderRadius:'0px'}}></td>
+                      <td style={{ background: '#faf0d2', borderTopColor: '#7c3271', borderLeft:'0px',borderRight:'0px', borderBottom:'0px', borderRadius:'0px' }}></td>
+                      <td style={{ background: '#faf0d2', fontFamily: 'Allerta, sans-serif', fontSize: 25, borderTopColor: '#7c3271', borderLeft:'0px',borderRight:'0px', borderBottom:'0px', borderRadius:'0px'}}>Total</td>
+                      <td style={{ background: '#faf0d2', fontFamily: 'Allerta, sans-serif', fontSize: 25, borderTopColor: '#7c3271', borderLeft:'0px',borderRight:'0px', borderBottom:'0px', borderRadius:'0px'}}>$00.00</td>
                     </tr>
                     <tr>
-                      <td style={{ background: '#FAF0D2' }}></td>
-                      <td style={{ background: '#FAF0D2' }}></td>
-                      <td style={{ background: '#FAF0D2' }}></td>
-                      <td style={{ background: '#FAF0D2' }}></td>
-                    </tr>
-                    <tr>
-                      <td style={{ background: '#faf0d2', borderTop: '2px solid #7c3271' }}></td>
-                      <td style={{ background: '#faf0d2', borderTop: '2px solid #7c3271' }}></td>
-                      <td style={{ background: '#faf0d2', fontFamily: 'Allerta, sans-serif', fontSize: 25, borderTop: '2px solid #7c3271' }}>Total</td>
-                      <td style={{ background: '#faf0d2', fontFamily: 'Allerta, sans-serif', fontSize: 25, borderTop: '2px solid #7c3271' }}>$00.00</td>
-                    </tr>
-                    <tr>
-                      <td style={{ background: '#faf0d2' }}></td>
-                      <td style={{ background: '#faf0d2' }}></td>
-                      <td style={{ background: '#faf0d2', fontFamily: 'Allerta, sans-serif', fontSize: 25 }}>Redondeo</td>
-                      <td style={{ background: '#faf0d2', fontFamily: 'Allerta, sans-serif', fontSize: 25 }}>$00.00</td>
+                      <td style={{ background: '#faf0d2', border:'none' }}></td>
+                      <td style={{ background: '#faf0d2', border:'none' }}></td>
+                      <td style={{ background: '#faf0d2', fontFamily: 'Allerta, sans-serif', fontSize: 25, border:'none'}}>Redondeo</td>
+                      <td style={{ background: '#faf0d2', fontFamily: 'Allerta, sans-serif', fontSize: 25, border:'none'}}>$00.00</td>
                     </tr>
                   </tbody>
                 </table>
